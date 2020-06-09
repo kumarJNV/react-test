4f697390ced6b4b6ac1c7219ca188ac4a12a3a0c
vijayk <kr.vijay47@gmail.com>
2020-06-09 21:24:25 +0530

    vijayk

---
+++ build/precache-manifest.06b8292b8642406c7b91efa80b6b889e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751835dc3825b43f483fe930a1a84b4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "621adc4f8f90f235d037",
+    "url": "/static/css/2.db19f6b7.chunk.css"
+  },
+  {
+    "revision": "6992885133c83ffbf756",
+    "url": "/static/css/main.85c80202.chunk.css"
+  },
+  {
+    "revision": "621adc4f8f90f235d037",
+    "url": "/static/js/2.57e228ed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.57e228ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6992885133c83ffbf756",
+    "url": "/static/js/main.cbef3190.chunk.js"
+  },
+  {
+    "revision": "68cca6af7ebc74efbdb7",
+    "url": "/static/js/runtime-main.39d31aef.js"
+  },
+  {
+    "revision": "1c91d2a7c3011bd103b85f4e06ba424e",
+    "url": "/static/media/desc.1c91d2a7.jpeg"
+  },
+  {
+    "revision": "fba44d7a0f88becee3ae67359acb8d2d",
+    "url": "/static/media/dog.fba44d7a.png"
+  },
+  {
+    "revision": "615ba1aee4ef9db468c5197c83a836bf",
+    "url": "/static/media/little.615ba1ae.png"
+  },
+  {
+    "revision": "d1b3b758df71fdb8965ed1b366fa76d8",
+    "url": "/static/media/mega.d1b3b758.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "8275ca748df392cdd9f981e0a324e1d8",
+    "url": "/static/media/slider1.8275ca74.jfif"
+  },
+  {
+    "revision": "b34b04f017d1e2d9b6f66620743d101d",
+    "url": "/static/media/slider2.b34b04f0.jfif"
+  },
+  {
+    "revision": "5f7a2f1775f7dc4784822807d6631e14",
+    "url": "/static/media/slider3.5f7a2f17.jfif"
+  },
+  {
+    "revision": "a7504baf0e099df8c767fa7d62d25b5b",
+    "url": "/static/media/smarso_logo.a7504baf.png"
+  },
+  {
+    "revision": "36c70af6703580fc1900a95cdacf64b3",
+    "url": "/static/media/tedd.36c70af6.png"
+  },
+  {
+    "revision": "50124c84ec0f1d25eed566d36608be8a",
+    "url": "/static/media/teddy.50124c84.jpg"
+  }
+]);
